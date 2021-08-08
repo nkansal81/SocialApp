a20bc1560f25149bfce5b0bd3a438569e3a3f9a2
nitika <nkansal81@gmail.com>
2021-08-08 17:02:37 +0530

    adding the files

--- src/actions/friends.js
+++ src/actions/friends.js
@@ -1,10 +1,6 @@
 import { APIUrls } from '../helpers/urls';
 import { getAuthTokenFromLocalStorage } from '../helpers/utils';
-import {
-  FETCH_FRIENDS_SUCCESS,
-  ADD_FRIEND,
-  REMOVE_FRIEND,
-} from './actionTypes';
+import { FETCH_FRIENDS_SUCCESS,ADD_FRIEND,REMOVE_FRIEND} from './actionTypes';
 
 export function fetchUserFriends(userId) {
   return (dispatch) => {
@@ -12,14 +8,14 @@ export function fetchUserFriends(userId) {
     fetch(url, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Bearer ${getAuthTokenFromLocalStorage()}`,
+        'Authorization': `Bearer ${getAuthTokenFromLocalStorage()}`,
       },
     })
       .then((response) => response.json())
       .then((data) => {
         console.log('data', data);
         dispatch(fetchFriendsSucces(data.data.friends));
-      });
+    });
   };
 }
 
@@ -30,16 +26,15 @@ export function fetchFriendsSucces(friends) {
   };
 }
 
-export function addFriend(friend) {
+export function addFriend(friend){
   return {
     type: ADD_FRIEND,
     friend,
   };
 }
-
-export function removeFriend(userId) {
+export function removeFriend(userId){
   return {
     type: REMOVE_FRIEND,
     userId,
-  };
+  }
 }
